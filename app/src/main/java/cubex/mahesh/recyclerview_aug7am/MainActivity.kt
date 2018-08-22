65ac0aca534e70e4df28c0dc92d03a66dd40ca8d
maheshthippala <mahesh.cubex@gmail.com>
2018-08-22 07:04:33 +0530

    Added gallery,GridView ,  fab & snackbar functionality

--- app/src/main/java/cubex/mahesh/recyclerview_aug7am/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/recyclerview_aug7am/MainActivity.kt
@@ -1,7 +1,10 @@
 package cubex.mahesh.recyclerview_aug7am
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,9 +13,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var lManager = LinearLayoutManager(this,
-                LinearLayoutManager.VERTICAL,false)
-        rview.layoutManager = lManager
+       /* var lManager = LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL,false)
+        rview.layoutManager = lManager */
+        var gManager = GridLayoutManager(this,2)
+        rview.layoutManager =  gManager
         rview.adapter = MyAdapter(this@MainActivity)
+        fab.setOnClickListener {
+          //  System.exit(0)
+            var sBar = Snackbar.make(it,"Are You Sure want to Exit?",
+                    Snackbar.LENGTH_INDEFINITE)
+            sBar.setAction("Yes",{
+                System.exit(0)
+            })
+            sBar.setActionTextColor(Color.RED)
+            sBar.show()
+        }
+
     }
 }
--- app/src/main/java/cubex/mahesh/recyclerview_aug7am/MyAdapter.kt
+++ app/src/main/java/cubex/mahesh/recyclerview_aug7am/MyAdapter.kt
@@ -1,26 +1,56 @@
 package cubex.mahesh.recyclerview_aug7am
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.ThumbnailUtils
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import java.io.File
 
-class MyAdapter:RecyclerView.Adapter<MyHolder>( )
+class MyAdapter:RecyclerView.Adapter<MyHolder>
 {
     var mActivity:MainActivity? = null
+    var files:Array<File>? = null
+    var file:File? = null
     constructor(mActivity:MainActivity){
             this.mActivity = mActivity
+       var path = "/storage/sdcard0/WhatsApp/Media/WhatsApp Images/"
+         file = File(path)
+        if(!file!!.exists()){
+            path = "/storage/emulated/0/WhatsApp/Media/WhatsApp Images/"
+            file = File(path)
+        }
+        files = file!!.listFiles()
     }
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MyHolder {
 
+        var inflater = LayoutInflater.from(mActivity)
+        var v = inflater.inflate(R.layout.indiview,p0,
+                false)
+        return  MyHolder(v)
     }
 
     override fun getItemCount(): Int {
-
+        return  files!!.size
       }
 
     override fun onBindViewHolder(p0: MyHolder, p1: Int) {
-
+            var f:File = files!![p1]
+            p0.name!!.text = f.name
+            p0.size!!.text = f.length().toString()
+            p0.del!!.setOnClickListener {
+                        f.delete()
+                files = file!!.listFiles()
+                this@MyAdapter.notifyDataSetChanged()
+            }
+            var bmp:Bitmap = BitmapFactory.decodeFile(f.path)
+            if(bmp!=null) {
+                var thumb_nail = ThumbnailUtils.extractThumbnail(bmp,
+                        50, 50)
+                p0.cView!!.setImageBitmap(thumb_nail)
+            }
       }
 
 }
